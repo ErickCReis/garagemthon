278bb48f621fd82681b24325413e53168d5b607d
ErickCReis <erickcorreareis@gmail.com>
2024-07-13 13:19:19 -0300

    chore: Remove lint no build

--- src/app/vehiculos/page.tsx
+++ src/app/vehiculos/page.tsx
@@ -1,10 +1,9 @@
 "use client";
 
-import React from "react";
-import { api } from "@/trpc/react";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
 import { MeioTransportIcon } from "@/app/_components/meio-transport-icon";
+import { Button } from "@/components/ui/button";
+import { api } from "@/trpc/react";
+import Link from "next/link";
 
 export default function VehiclesPage() {
   const { data: vehicles, isLoading } = api.veiculos.getAll.useQuery();
@@ -17,21 +16,39 @@ export default function VehiclesPage() {
     <main className="flex min-h-screen flex-col p-4">
       <section className="space-y-4">
         <h2 className="text-center text-2xl font-bold">Veículos</h2>
-        <h3 className="text-center text-2xl">Veículos disponíveis para transporte</h3>
+        <h3 className="text-center text-2xl">
+          Veículos disponíveis para transporte
+        </h3>
         <div className="flex flex-col space-y-2">
           {vehicles?.map(({ vehicle, owner }) => (
-            <div key={vehicle.id} className="flex items-center gap-4 p-4 border rounded-md">
-              <MeioTransportIcon meio={vehicle.type as any} className="w-16 h-16" />
+            <div
+              key={vehicle.id}
+              className="flex items-center gap-4 rounded-md border p-4"
+            >
+              <MeioTransportIcon
+                meio={vehicle.type as any}
+                className="h-16 w-16"
+              />
               <div>
                 <h3 className="text-xl font-semibold">{vehicle.type}</h3>
                 <p>{vehicle.description}</p>
-                <p><strong>ID:</strong> {vehicle.vehicleId}</p>
-                <p><strong>Modelo:</strong> {vehicle.model}</p>
-                <p><strong>Cor:</strong> {vehicle.color}</p>
+                <p>
+                  <strong>ID:</strong> {vehicle.vehicleId}
+                </p>
+                <p>
+                  <strong>Modelo:</strong> {vehicle.model}
+                </p>
+                <p>
+                  <strong>Cor:</strong> {vehicle.color}
+                </p>
                 {owner && (
                   <>
-                    <p><strong>Proprietário:</strong> {owner.nome}</p>
-                    <p><strong>Email:</strong> {owner.email}</p>
+                    <p>
+                      <strong>Proprietário:</strong> {owner.nome}
+                    </p>
+                    <p>
+                      <strong>Email:</strong> {owner.email}
+                    </p>
                   </>
                 )}
               </div>
